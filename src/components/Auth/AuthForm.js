bac418a9eb7dd10f6dae9db27137b29620b37a7c
Kürşad Bumin Giray Saka <kbgs.1903@gmail.com>
2022-09-24 19:34:19 +0300

    add sending login request to firebase

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -22,35 +22,42 @@ const AuthForm = () => {
 		// optional: Add validation
 
 		setIsLoading(true);
-		if (isLogin) {
-		} else {
-			fetch(
-				'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCpvumBfmJxEVqf8wncjmcHIqyUk-UanoU',
-				{
-					method: 'POST',
-					body: JSON.stringify({
-						email: enteredEmail,
-						password: enteredPassword,
-						returnSecureToken: true,
-					}),
-					headers: {
-						'Content-Type': 'application/json',
-					},
-				}
-			).then((res) => {
+
+		const url = isLogin
+			? 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCpvumBfmJxEVqf8wncjmcHIqyUk-UanoU'
+			: 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCpvumBfmJxEVqf8wncjmcHIqyUk-UanoU';
+
+		fetch(url, {
+			method: 'POST',
+			body: JSON.stringify({
+				email: enteredEmail,
+				password: enteredPassword,
+				returnSecureToken: true,
+			}),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		})
+			.then((res) => {
 				setIsLoading(false);
 				if (res.ok) {
+					return res.json();
 				} else {
 					return res.json().then((data) => {
 						let errorMessage = 'Authentication failed!';
 						// if (data && data.error && data.error.message) {
 						// 	errorMessage = data.error.message;
 						// }
-						alert(errorMessage);
+						throw new Error(errorMessage);
 					});
 				}
+			})
+			.then((data) => {
+				console.log(data);
+			})
+			.catch((err) => {
+				alert(err.message);
 			});
-		}
 	};
 
 	return (
